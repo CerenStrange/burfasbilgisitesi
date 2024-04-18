4465c7285a9935f3afc26cebd02be9026a7e6a8e
Ceren Çelik <157277848+CerenStrange@users.noreply.github.com>
2024-04-18 14:07:21 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ function Navbar() {
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collaps navbar-collapse" id="navbarText">
+          <div className="collapse navbar-collapse" id="navbarText">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <NavLink className="nav-link" to="/" >ANASAYFA</NavLink>
               <NavLink className="nav-link" to="/subeler" >ŞUBELER</NavLink>
